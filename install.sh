f7259f3459f0019ead35d24ea459f09282c41e34
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2021-10-14 16:06:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
-bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
+wget -O install-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+bash install-release.sh
+rm install-release.sh
+wget -O install-dat-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
+bash install-dat-release.sh
+rm install-dat-release.sh
