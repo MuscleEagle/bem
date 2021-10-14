9150ee3590c929c2d7d4bf4ba1a1e7192f5be0eb
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2021-10-14 14:12:44 +0800

    Create go.sh

--- go.sh
+++ go.sh
@@ -5,28 +5,20 @@ YourCachePath="cache"
 NginxDefaultFile="/etc/nginx/conf.d/default.conf"
 NginxCustomFile="/etc/nginx/conf.d/custom.conf"
 
-echo "Set Nginx Default Config"
-read ContinueDefaultConfig
-if(($ContinueDefaultConfig == "Y") || ($ContinueDefaultConfig == "y"))
-{
-  rm -f $NginxDefaultFile
-  wget -O $NginxDefaultFile https://raw.githubusercontent.com/MuscleEagle/zzinzzuptest/main/default.conf
-  sed -i "s/YourDomainName/${YourDomain}/g" $NginxDefaultFile
-}else{
-  exit 1
-}
+echo "Delete Old Nginx Default Config File!"
+rm -f $NginxDefaultFile
+echo "Download default.conf"
+wget -O $NginxDefaultFile https://raw.githubusercontent.com/MuscleEagle/zzinzzuptest/main/default.conf
+echo "Fill Custom Setting"
+sed -i "s/YourDomainName/${YourDomain}/g" $NginxDefaultFile
 
 certbot certonly --webroot -w /usr/share/nginx/html/ --email admin@$YourDomain -d $YourDomain
 
-echo "Set Nginx Custom Config"
-read ContinueCustomConfig
-if(($ContinueCustomConfig == "Y") || ($ContinueCustomConfig == "y"))
-{
-  rm -f $NginxCustomFile
-  wget -O $NginxCustomFile https://raw.githubusercontent.com/MuscleEagle/zzinzzuptest/main/new.conf
-  sed -i "s/YourDomainName/${YourDomain}/g" $NginxCustomFile
-  sed -i "s/YourCachePath/${YourCachePath}/g" $NginxCustomFile
-  sed -i "s/YourPort/${YourPort}/g" $NginxCustomFile
-}else{
-  exit 1
-}
+echo "Delete Old Nginx Custom Config File!"
+rm -f $NginxCustomFile
+echo "Download custom.conf"
+wget -O $NginxCustomFile https://raw.githubusercontent.com/MuscleEagle/zzinzzuptest/main/new.conf
+echo "Fill Custom Setting"
+sed -i "s/YourDomainName/${YourDomain}/g" $NginxCustomFile
+sed -i "s/YourCachePath/${YourCachePath}/g" $NginxCustomFile
+sed -i "s/YourPort/${YourPort}/g" $NginxCustomFile
