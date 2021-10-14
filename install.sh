67a313907632cd721601a1784b249ba78ecbc85e
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2021-10-14 19:06:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
+apt update
+apt install ntpdate
+apt install wget
+apt install curl
+apt install gcc
+apt install nginx
+apt install certbot
 wget -O install-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 bash install-release.sh
 rm install-release.sh
 wget -O install-dat-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
 bash install-dat-release.sh
 rm install-dat-release.sh
-
-{58F390A9-F015-4863-A235-CA4A748FE540}
-{E77A9FF4-F88D-4A6D-9D09-4BFDEEB01D92}
-{75B2B830-AFAD-4ED5-BCC9-0612737FF2A2}
-{E65119B4-AF99-447D-943C-DA561983DDAC}
-{63E6307F-6720-4D6C-90DB-D3CE0D36E701}
-{DF4BB618-04E2-4487-BE84-CE7E5779DD10}
-{38EC67C1-00D3-4DDB-BCB0-271F44929802}
-{0D8EA3C0-E2FD-4D34-9BA9-6F16BCD79BE7}
