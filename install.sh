603f22fd82d6887f3c193549e5a5edfe4938b21d
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2021-10-14 19:16:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Install dependencies and correct time！"
 apt update
-apt install ntpdate
-apt install wget
-apt install curl
-apt install gcc
-apt install nginx
-apt install certbot
+apt install ntpdate -y
+apt install wget -y
+apt install curl -y
+apt install gcc -y
+apt install nginx -y
+apt install certbot -y
 wget -O install-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 bash install-release.sh
 rm install-release.sh
