e24e1612680930f39dd9ed06a658605e599971c8
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2021-10-14 16:03:15 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
