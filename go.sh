b30941169bc980e1995fc1eed970ad02caf7756e
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2021-10-14 15:06:54 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-YourDomain="xxx.com"
-YourPort="6543"
-YourCachePath="cache"
 NginxDefaultFile="/etc/nginx/conf.d/default.conf"
 NginxCustomFile="/etc/nginx/conf.d/custom.conf"
 
-echo "Delete Old Nginx Default Config File!"
-rm -f $NginxDefaultFile
-echo "Download default.conf"
-wget -O $NginxDefaultFile https://raw.githubusercontent.com/MuscleEagle/zzinzzuptest/main/default.conf
-echo "Fill Custom Setting"
-sed -i "s/YourDomainName/${YourDomain}/g" $NginxDefaultFile
-
-certbot certonly --webroot -w /usr/share/nginx/html/ --email admin@$YourDomain -d $YourDomain
+echo "Input Your Domain Name:"
+read YourDomain
+echo "Input Your Port:"
+read YourPort
+echo "Input Your Cache Path:"
+read YourCachePath
 
 echo "Delete Nginx Default Config File!"
 rm -f $NginxDefaultFile
