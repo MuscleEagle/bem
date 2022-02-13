a95ea4cc9579d4c390a5ef8735d62f6570c514d3
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-02-13 13:13:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Install dependencies and correct time！"
+echo "Install dependencies..."
 apt update
 apt install ntpdate -y
 apt install wget -y
@@ -7,6 +7,9 @@ apt install curl -y
 apt install gcc -y
 apt install nginx -y
 apt install certbot -y
+echo "Correct time..."
+ntpdate cn.pool.ntp.org
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 wget -O install-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 bash install-release.sh
 rm install-release.sh
