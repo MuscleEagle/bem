6d1ea199774de37984e7eccfa84190bc5c12d0d0
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-08-22 16:13:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,69 @@
 #!/bin/bash
-echo "Install dependencies..."
-apt update
-apt install vim -y
-apt install ntpdate -y
-apt install wget -y
-apt install curl -y
-apt install gcc -y
-apt install uuid-dev
-apt install nginx -y
-apt install certbot -y
-echo "Correct time..."
-ntpdate cn.pool.ntp.org
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+read -r -p "Install dependencies?  [Y/n] " inputA
+case $inputA in
+    [yY][eE][sS]|[yY])
+		echo "Yes"
+		apt update
+		apt install vim -y
+		apt install ntpdate -y
+		apt install wget -y
+		apt install curl -y
+		apt install gcc -y
+		apt install uuid-dev
+		apt install nginx -y
+		apt install certbot -y
+		;;
+
+    [nN][oO]|[nN])
+		echo "No"
+       		;;
+
+    *)
+		echo "Invalid input..."
+		exit 1
+		;;
+esac
+
+read -r -p "Correct time?  [Y/n] " inputB
+case $inputB in
+    [yY][eE][sS]|[yY])
+		echo "Yes"
+		ntpdate cn.pool.ntp.org
+		cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+		;;
+
+    [nN][oO]|[nN])
+		echo "No"
+       		;;
+
+    *)
+		echo "Invalid input..."
+		exit 1
+		;;
+esac
+
 echo "Create bemray project..."
 wget -O bemray.c https://raw.githubusercontent.com/MuscleEagle/bem/main/bemray.c
 gcc bemray.c -luuid -o bemray
 echo "Run bemray..."
 ./bemray
-bash dependencies.sh
-bash modifyconfig.sh
+
+read -r -p "Run bash?  [Y/n] " inputC
+case $inputC in
+    [yY][eE][sS]|[yY])
+		echo "Yes"
+		bash dependencies.sh
+		bash modifyconfig.sh
+		;;
+
+    [nN][oO]|[nN])
+		echo "No"
+       		;;
+
+    *)
+		echo "Invalid input..."
+		exit 1
+		;;
+esac
+
 echo "Install finish！"
