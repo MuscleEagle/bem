6e5acada93cf9481ff33d5a83be915f45b326373
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-08-22 17:08:31 +0800

    Create Project.sh

---
+++ Project.sh
@@ -0,0 +1,8 @@
+echo "Install YourProject!"
+wget -O install-release.sh https://raw.githubusercontent.com/YourProject/fhs-install-YourProject/master/install-release.sh
+wget -O install-dat-release.sh https://raw.githubusercontent.com/YourProject/fhs-install-YourProject/master/install-dat-release.sh
+bash install-release.sh
+rm install-release.sh
+bash install-dat-release.sh
+rm install-dat-release.sh
+echo "YourProject is ready!"
