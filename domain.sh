9be27b465aa1a216762b696c2f3d02cb172a4bff
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-08-22 17:07:25 +0800

    Update domain.sh

--- domain.sh
+++ domain.sh
@@ -14,12 +14,12 @@ case $input in
 		exit 1
 		;;
 esac
-read -r -p "Please enter the domain name: " DomainName
+
 NginxDefaultFile="/etc/nginx/conf.d/default.conf"
 
 echo "server {" > $NginxDefaultFile
 echo "    listen        80;" >> $NginxDefaultFile
-echo "    server_name   ${DomainName};" >> $NginxDefaultFile
+echo "    server_name   YourDomain;" >> $NginxDefaultFile
 echo "    location / {" >> $NginxDefaultFile
 echo "        root   /usr/share/nginx/html;" >> $NginxDefaultFile
 echo "        index  index.html index.htm;" >> $NginxDefaultFile
@@ -29,5 +29,9 @@ echo "    location = /50x.html {" >> $NginxDefaultFile
 echo "        root   /usr/share/nginx/html;" >> $NginxDefaultFile
 echo "    }" >> $NginxDefaultFile
 echo "}" >> $NginxDefaultFile
-certbot certonly --quiet --non-interactive --agree-tos --webroot -w /usr/share/nginx/html/ --email admin@$DomainName -d $DomainName
-certbot renew  --quiet  --no-self-upgrade
+
+echo "Install ACME!"
+wget -O -  https://get.acme.sh | sh -s email=admin@YourDomain
+/root/.acme.sh/acme.sh --issue -d YourDomain --nginx
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+echo "ACME is ready!"
