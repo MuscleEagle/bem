aebaac87015b2452b2eda9bac3f2c1bca8300834
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-08-23 09:28:57 +0800

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,6 +1,6 @@
 echo "Install YourProject!"
-wget -O install-release.sh https://raw.githubusercontent.com/YourProjectSwitch/fhs-install-YourProject/master/install-release.sh
-wget -O install-dat-release.sh https://raw.githubusercontent.com/YourProjectSwitch/fhs-install-YourProject/master/install-dat-release.sh
+wget -O install-release.sh https://raw.githubusercontent.com/YourSwitch/fhs-install-YourProject/master/install-release.sh
+wget -O install-dat-release.sh https://raw.githubusercontent.com/YourSwitch/fhs-install-YourProject/master/install-dat-release.sh
 bash install-release.sh
 rm install-release.sh
 bash install-dat-release.sh
