4fd18ec7aada3bb4b73bf9cb05c5f58a7abfd828
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-10-04 16:22:20 +0800

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,4 +1,4 @@
 echo "Install YourProject!"
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install-geodata
+bash -c "$(curl -L https://github.com/YourSwitch/YourProject-install/raw/main/install-release.sh)" @ install
+bash -c "$(curl -L https://github.com/YourSwitch/YourProject-install/raw/main/install-release.sh)" @ install-geodata
 echo "YourProject is ready!"
