d0683b231668f21cadb7cfa82bf6d56e24b83a85
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-10-05 17:26:46 +0800

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,4 +1,4 @@
 echo "Install YourProject!"
-bash -c "$(curl -L https://github.com/AuthorName/ProjectName-install/raw/main/install-release.sh)" @ install
-bash -c "$(curl -L https://github.com/AuthorName/ProjectName-install/raw/main/install-release.sh)" @ install-geodata
+bash -c "$(curl -L https://github.com/AuthorName/fhs-install-ProjectName/raw/main/install-release.sh)" @ install
+bash -c "$(curl -L https://github.com/AuthorName/fhs-install-ProjectName/raw/main/install-release.sh)" @ install-geodata
 echo "YourProject is ready!"
