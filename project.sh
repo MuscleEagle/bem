a876de0ba6093809df656d2dd8b61e2b61438300
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2022-10-05 17:58:35 +0800

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,4 +1,3 @@
 echo "Install ProjectName!"
 bash <(curl -L https://raw.githubusercontent.com/AuthorName/fhs-install-ProjectName/master/install-release.sh) --version v4.45.2
-bash <(curl -L https://raw.githubusercontent.com/AuthorName/fhs-install-ProjectName/master/install-dat-release.sh)
 echo "ProjectName is ready!"
