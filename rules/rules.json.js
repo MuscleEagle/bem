9b85181abc379e02af7ec48fb45a7a6d7f34b079
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2023-11-04 10:43:22 +0800

    Create rules.json.js

---
+++ rules/rules.json.js
@@ -0,0 +1,39 @@
+{
+  "gfwlist": {
+    "name": "gfwlist.conf",
+    "date": "2023-09-18 03:49",
+    "md5": "0000",
+    "count": "6024"
+  },
+  "chnroute": {
+    "name": "chnroute.txt",
+    "date": "2023-11-03 03:49",
+    "md5": "0000",
+    "count": "3436",
+    "count_ip": "296263720"
+  },
+  "cdn_china": {
+    "name": "cdn.txt",
+    "date": "2023-11-03 03:49",
+    "md5": "0000",
+    "count": "63610"
+  },
+  "apple_china": {
+    "name": "apple_china.txt",
+    "date": "2023-05-29 03:49",
+    "md5": "0000",
+    "count": "140"
+  },
+  "google_china": {
+    "name": "google_china.txt",
+    "date": "2023-04-23 03:49",
+    "md5": "0000",
+    "count": "159"
+  },
+  "cdn_test": {
+    "name": "cdn_test.txt",
+    "date": "2023-07-09 03:49",
+    "md5": "0000",
+    "count": "82"
+  }
+}
