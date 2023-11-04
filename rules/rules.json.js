d03378269d76703626d35b054bc4b4d2f19c9bd0
MuscleEagle <54014829+MuscleEagle@users.noreply.github.com>
2023-11-04 11:19:34 +0800

    Update rules.json.js

--- rules/rules.json.js
+++ rules/rules.json.js
@@ -2,38 +2,38 @@
   "gfwlist": {
     "name": "gfwlist.conf",
     "date": "2023-09-18 03:49",
-    "md5": "0000",
+    "md5": "5bcf85f069ab6c4f7c8a2e195579f7b1",
     "count": "6024"
   },
   "chnroute": {
     "name": "chnroute.txt",
     "date": "2023-11-03 03:49",
-    "md5": "0000",
+    "md5": "c23abdf8fd1ab6416a2432136e34cc61",
     "count": "3436",
     "count_ip": "296263720"
   },
   "cdn_china": {
     "name": "cdn.txt",
     "date": "2023-11-03 03:49",
-    "md5": "0000",
+    "md5": "dd8de4824a05766edf30f5475190b38c",
     "count": "63610"
   },
   "apple_china": {
     "name": "apple_china.txt",
     "date": "2023-05-29 03:49",
-    "md5": "0000",
+    "md5": "a93bb459f62e0e550200b5f85304fd52",
     "count": "140"
   },
   "google_china": {
     "name": "google_china.txt",
     "date": "2023-04-23 03:49",
-    "md5": "0000",
+    "md5": "2acb00542a8e264052db7a42b3a693d8",
     "count": "159"
   },
   "cdn_test": {
     "name": "cdn_test.txt",
     "date": "2023-07-09 03:49",
-    "md5": "0000",
+    "md5": "903ac79bac193dc59cb1eed69c7d6cc6",
     "count": "82"
   }
 }
